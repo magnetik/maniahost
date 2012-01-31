b88f7f4f744e8764c02f0477784ef49dfa8e99d8
melot.philippe <melot.philippe@gmail.com>
2012-01-31 14:51:39 +0000

    Change ManiaLive Plugin default configuration remove debug log entry

--- libraries/ManiaLivePlugins/ManiaHost/Manager/Config.php
+++ libraries/ManiaLivePlugins/ManiaHost/Manager/Config.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @copyright   Copyright (c) 2009-2012 NADEO (http://www.nadeo.com)
  * @license     http://www.gnu.org/licenses/lgpl.html LGPL License 3
@@ -7,16 +6,28 @@
  * @author      $Author$:
  * @date        $Date$:
  */
+
 namespace ManiaLivePlugins\ManiaHost\Manager;
 
 class Config extends \ManiaLib\Utils\Singleton
 {
-	public $host = '127.0.0.1';
-	public $username = 'root';
-	public $password = '';
-	public $database = 'ManiaHost';
+
+	public $host;
+	public $username;
+	public $password;
+	public $database;
 	public $type = 'MySQL';
 	public $port = 3306;
+
+	protected function __construct()
+	{
+		$config = \ManiaLive\Database\Config::getInstance();
+		$this->host = $config->host;
+		$this->username = $config->username;
+		$this->password = $config->password;
+		$this->database = $config->database;
+	}
+
 }
 
 ?>
